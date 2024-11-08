c94a8c7d4e2f2476256ec74885b522a35eac704e
al-jabir <aljabir098@gmail.com>
2024-11-09 01:02:18 +0600

    update sizes chips

---
+++ src/app/(protected)/components/chip/SizeChip.tsx
@@ -0,0 +1,12 @@
+import { Box, Chip } from '@mui/material';
+
+const SizeChip = () => {
+  return (
+    <Box display="flex" gap="12px" flexWrap="wrap">
+      <Chip label="Small" size="small" />
+      <Chip label="Small" size="small" variant="outlined" />
+    </Box>
+  );
+};
+
+export default SizeChip;
--- src/app/(protected)/components/chip/page.tsx
+++ src/app/(protected)/components/chip/page.tsx
@@ -1,21 +1,25 @@
-import Card from "@/components/ui/CodeSnippet";
-import { Box } from "@mui/material";
-import ColorChip from "./ColorChip";
+import Card from '@/components/ui/CodeSnippet';
+import { Box } from '@mui/material';
+import ColorChip from './ColorChip';
+import SizeChip from './SizeChip';
 
 const ChipPage = () => {
   return (
     <Box
       sx={{
-        display: "grid",
+        display: 'grid',
         gridTemplateColumns: {
-          xs: "repeat(1, 1fr)",
-          lg: "repeat(2, 1fr)",
+          xs: 'repeat(1, 1fr)',
+          lg: 'repeat(2, 1fr)',
         },
         gap: 2,
       }}
     >
       <Card title="Color">
         <ColorChip />
+      </Card>
+      <Card title="Sizes">
+        <SizeChip />
       </Card>
 
       {/* 
