f0d51ec35e26595b1d41600803c592b259d68eee
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2024-11-09 16:52:11 +0600

    add: accordion page

---
+++ src/app/(protected)/components/accordion/AccordionExpandIcon.tsx
@@ -0,0 +1,51 @@
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { ArrowDownIcon } from "lucide-react";
+const AccordionExpandIcon = () => {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDownIcon size={20} />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Accordion 1
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDownIcon size={20} />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          Accordion 2
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDownIcon size={20} />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Accordion 3
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
+
+export default AccordionExpandIcon;
--- src/app/(protected)/components/accordion/page.tsx
+++ src/app/(protected)/components/accordion/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
-const Accordion = () => {
+import Card from "@/components/ui/CodeSnippet";
+import { Box } from "@mui/material";
+import BasicAccordion from "./BasicAccordion";
+import AccordionExpandIcon from "./AccordionExpandIcon";
+import ExpandedAccordion from "./ExpandedAccordion";
+import DisabledAccordion from "./DisabledAccordion";
+import ControlledAccordions from "./ControlledAccordion";
+import AccordionAction from "./AccordionAction";
+const AccordionPage = () => {
   return (
-    <div>Accordion</div>
-  )
-}
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: {
+          xs: "repeat(1, 1fr)",
+          lg: "repeat(2, 1fr)",
+        },
+        gap: 2,
+      }}
+    >
+      <Card title="Basic">
+        <BasicAccordion />
+      </Card>
+      <Card title="Control Expand Icon">
+        <AccordionExpandIcon />
+      </Card>
+      <Card title="Expanded Accordion">
+        <ExpandedAccordion />
+      </Card>
+      <Card title="Disabled Item">
+        <DisabledAccordion />
+      </Card>
+      <Card title="Controlled Accordion">
+        <ControlledAccordions />
+      </Card>
+      <Card title=" Accordion Action">
+        <AccordionAction />
+      </Card>
+    </Box>
+  );
+};
 
-export default Accordion
+export default AccordionPage;
